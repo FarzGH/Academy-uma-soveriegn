a8ac45574806da3d03aa0e6bb2a5fbc84455e31b
FarzGH <aligthforfarz@gmail.com>
2026-04-19 09:58:28 +0900

    change 10 second

--- animation.js
+++ animation.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function() {
         strings: ['Academy Uma Sovereign', 'A\'ueS Community', 'Sovereignty & Unity'],
         typeSpeed: 60,
         backSpeed: 40,
-        backDelay: 60000, // Menunggu 1 menit (60000ms) sebelum dihapus
+        backDelay: 10000, 
         loop: true,
         showCursor: true,
         cursorChar: '_'
